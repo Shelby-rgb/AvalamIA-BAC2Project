2b40b48146ae4d77859e426f5815d9c35f3f41a5
Walibi420 <60740674+Walibi420@users.noreply.github.com>
2020-05-05 10:46:43 +0200

    Add server file + readme

---
+++ battle_server.py
@@ -0,0 +1,32 @@
+import cherrypy
+import sys
+from AI_runner_version import AI_runner
+
+class Server:
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def move(self):
+        # Deal with CORS
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
+        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+        if cherrypy.request.method == "OPTIONS":
+            return ''
+        
+        body = cherrypy.request.json
+        print(body)
+        return AI_runner(state=body, depth=2)
+
+    @cherrypy.expose
+    def ping(self):
+        return "Ave, Caesar, morituri te salutant"
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        port=int(sys.argv[1])
+    else:
+        port=8080
+
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': port})
+    cherrypy.quickstart(Server())
