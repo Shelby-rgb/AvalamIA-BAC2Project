e6b32e00ff569e51e38f1fc39c9d60d9b0640ba9
Robin Lamotte <17051@ecam.be>
2020-05-06 02:54:18 +0200

    Add teacher's json transfer method

--- Inscription.py
+++ Inscription.py
@@ -12,13 +12,23 @@ msg = {"matricules": matricules,
        "port": port,
        "name": name}
 
-msg = json.dumps(msg)
-msg = bytes(msg, "utf-8")
+def sendJSON(socket, data):
+	msg = json.dumps(data).encode('utf8')
+	total = 0
+	while total < len(msg):
+		sent = socket.send(msg[total:])
+		total += sent
+def recvJSON(socket):
+	finished = False
+	msg = ''
+	while not finished:
+		msg += socket.recv(4096).decode('utf8')
+		try:
+			data = json.loads(msg)
+			finished = True
+		except json.JSONDecodeError:
+			pass
+	return data
 
-tot = 0
-while tot < len(msg):
-       sent = s.send(msg[tot:])
-       tot += sent
-#s.send(msg)
-rep = s.recv(1024)
-print(rep)
+sendJSON(s, msg)
+print(recvJSON(s))
